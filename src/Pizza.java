8af3073c514219f3556a39e284813a27e4ee6519
Travinovs <aleksandrs.travinovs@gmail.com>
2023-03-14 09:18:22 +0200

    klasē Pizza izveidots konstruktors un metodes

--- src/Pizza.java
+++ src/Pizza.java
@@ -1,4 +1,32 @@
 
 public class Pizza {
-
+	private int size;
+	private String type;
+	private String topping;
+	
+	public Pizza(int size, String type, String topping) {
+		this.size = size;
+		this.type = type;
+		this.topping = topping;
+	}
+	
+	public void setSize(int cm) {
+		size = cm;
+	}
+	
+	public int getSize() {
+		return size;
+	}
+	
+	public void setType(String pizzaType) {
+		type = pizzaType;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+	public void setTopping(String pizzaTopping) {
+		topping = pizzaTopping;
+	}
 }
